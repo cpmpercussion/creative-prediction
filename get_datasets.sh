231cb9590d2f6747a61742d8528d844eb56083ac
Charles Martin <cpm@charlesmartin.com.au>
2018-07-18 00:19:51 +1000

    adding in Robojam notebook

---
+++ get_datasets.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir datasets
+cd datasets
+wget http://folk.uio.no/charlepm/datasets/TinyPerformanceCorpus.h5
+wget http://folk.uio.no/charlepm/datasets/MetatoneTinyPerformanceRecords.h5
+cd ..
