03e73cfeeab4e9cc5a90cb44007655ad095b93e7
Charles Martin <cpm@charlesmartin.com.au>
2021-01-19 23:10:23 +1100

    updated with new url

--- get_datasets.sh
+++ get_datasets.sh
@@ -2,6 +2,6 @@
 
 mkdir datasets
 cd datasets
-wget http://folk.uio.no/charlepm/datasets/TinyPerformanceCorpus.h5
-wget http://folk.uio.no/charlepm/datasets/MetatoneTinyPerformanceRecords.h5
+wget https://metatonetransfer.com/datasets/TinyPerformanceCorpus.h5
+wget https://metatonetransfer.com/datasets/MetatoneTinyPerformanceRecords.h5
 cd ..
